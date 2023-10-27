1ea089a63172b2ddb493f4283c8e8bde653917fb
MaxPredathor <max.gilli98@gmail.com>
2023-10-27 15:01:07 +0200

    added image and background image trought vue

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      title: "My wallpaper",
+      myColor: "text-primary fs-1 text-center",
+      myImage: "../img/vue-blog-cover-2.webp",
+      mainStyle:
+        "background-image: url(../img/my-wallpaper.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center",
+    };
+  },
+}).mount("#app");
