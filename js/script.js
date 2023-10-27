989a8d78a03846b4197beb0d9f9a6d01020d47d1
MaxPredathor <max.gilli98@gmail.com>
2023-10-27 15:05:59 +0200

    small fix

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ createApp({
     return {
       title: "My wallpaper",
       myColor: "text-primary fs-1 text-center",
-      myImage: "../img/vue-blog-cover-2.webp",
+      myImage: "img/vue-blog-cover-2.webp",
       mainStyle:
-        "background-image: url(../img/my-wallpaper.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center",
+        "background-image: url(img/my-wallpaper.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center",
     };
   },
 }).mount("#app");
